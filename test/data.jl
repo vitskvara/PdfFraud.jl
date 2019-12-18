b6dbbad25140c707241faf9adbf9397541188042
Vít Škvára <vit.skvara@gmail.com>
2019-12-18 14:22:59 +0100

    test

--- test/data.jl
+++ test/data.jl
@@ -1,5 +1,5 @@
 @testset "data" begin
-	joinpath(PdfFraud.proto_path, "protos/ff953f62f5739a910f96465c34367e31f6f4fdc112b8a27f66a7d88dd519ec91.pb")
+	fs = joinpath(PdfFraud.proto_path, "protos/ff953f62f5739a910f96465c34367e31f6f4fdc112b8a27f66a7d88dd519ec91.pb")
 
 	pdf = PdfFraud.read_pb(fs);
 	@test typeof(pdf) == PdfSampleRaw
