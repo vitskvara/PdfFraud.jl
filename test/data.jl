be3a6a87a3935697f3564dcaf54b4d2fbb7380f6
Vít Škvára <vit.skvara@gmail.com>
2019-12-18 14:19:02 +0100

    fixed test path

--- test/data.jl
+++ test/data.jl
@@ -1,5 +1,5 @@
 @testset "data" begin
-	fs = "/home/vit/vyzkum/bulleproof/pdf_experiment/data/uloz_to_2019_11_05/protos/ff953f62f5739a910f96465c34367e31f6f4fdc112b8a27f66a7d88dd519ec91.pb"
+	joinpath(PdfFraud.proto_path, "protos/ff953f62f5739a910f96465c34367e31f6f4fdc112b8a27f66a7d88dd519ec91.pb")
 
 	pdf = PdfFraud.read_pb(fs);
 	@test typeof(pdf) == PdfSampleRaw
@@ -7,13 +7,3 @@
 	@test length(vs) == 8
 	xs = map(x -> reshape(x, 64, 48), vs) 	
 end
-
-"
-using PyPlot
-
-figure()
-for i in 1:8
-	subplot(8,1,i)
-	pcolormesh(xs[i])
-end
-"
