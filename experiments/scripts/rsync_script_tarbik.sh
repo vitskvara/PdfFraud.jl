d6d0c0af104835e577169698f9eace5e774fd035
Vit Skvara <skvara@tarbik.utia.cas.cz>
2020-01-07 15:49:07 +0100

    tarbik

---
+++ experiments/scripts/rsync_script_tarbik.sh
@@ -0,0 +1,2 @@
+rsync -a ~/work/bulletproof/pdf_experiment/experiments/ skvara@ltserv3.tok.ipp.cas.cz:vyzkum/bulletproof/pdf_experiment/experiments/
+
---
+++ experiments/scripts/train_tarbik.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+LDIM=128
+NEPOCHS=100
+SVPTH=vae_runs/${LDIM}_3
+SF=10
+JULIA_DEPOT_PATH=/home/skvara/.julia /home/skvara/julia-1.3.1/bin/julia ../run_experiment.jl $LDIM 32 32 64 --savepath=$SVPTH --nepochs=$NEPOCHS --batchsize=128 --eta=0.0005 --save-frequency=$SF
